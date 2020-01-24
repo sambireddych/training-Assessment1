74170eb83ad4c31205e5e5dc664b8412e1366727
sambireddych <42915113+sambireddych@users.noreply.github.com>
2020-01-24 10:12:34 -0500

    Update Driver.java

--- src/main/java/Assessment1/Driver.java
+++ src/main/java/Assessment1/Driver.java
@@ -8,9 +8,8 @@ public class Driver {
 	public static void main(String[] args) {
 		Scanner scaner = new Scanner(System.in);
 		System.out.println("Please enter the name: ");
-		String name;
+		String name = scaner.nextLine();;
 		while (true) {
-			name = scaner.nextLine();
 			if (name == null || name.equals("")) {
 				System.out.println("Please enter a name again: ");
 				name = scaner.nextLine();
